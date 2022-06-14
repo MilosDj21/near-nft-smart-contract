52a1c5a107e44c8d14ca6dd7163a33e919c0b5a4
Ikaraz <m.djuka@hotmail.com>
2022-06-14 13:54:12 +0200

    Init

---
+++ nft-contract/src/approval.rs
@@ -0,0 +1,146 @@
+use crate::*;
+use near_sdk::{ext_contract, Gas};
+
+const GAS_FOR_NFT_APPROVE: Gas = Gas(10_000_000_000_000);
+const NO_DEPOSIT: Balance = 0;
+
+pub trait NonFungibleTokenCore {
+    //approve an account ID to transfer a token on your behalf
+    fn nft_approve(&mut self, token_id: TokenId, account_id: AccountId, msg: Option<String>);
+
+    //check if the passed in account has access to approve the token ID
+	fn nft_is_approved(
+        &self,
+        token_id: TokenId,
+        approved_account_id: AccountId,
+        approval_id: Option<u64>,
+    ) -> bool;
+
+    //revoke a specific account from transferring the token on your behalf
+    fn nft_revoke(&mut self, token_id: TokenId, account_id: AccountId);
+
+    //revoke all accounts from transferring the token on your behalf
+    fn nft_revoke_all(&mut self, token_id: TokenId);
+}
+
+#[ext_contract(ext_non_fungible_approval_receiver)]
+trait NonFungibleTokenApprovalsReceiver {
+    //cross contract call to an external contract that is initiated during nft_approve
+    fn nft_on_approve(
+        &mut self,
+        token_id: TokenId,
+        owner_id: AccountId,
+        approval_id: u64,
+        msg: String,
+    );
+}
+
+#[near_bindgen]
+impl NonFungibleTokenCore for Contract {
+
+    //allow a specific account ID to approve a token on your behalf
+    #[payable]
+    fn nft_approve(&mut self, token_id: TokenId, account_id: AccountId, msg: Option<String>) {
+        
+        assert_at_least_one_yocto();
+
+        let mut token = self.tokens_by_id.get(&token_id).expect("No token");
+
+        assert_eq!(
+            &env::predecessor_account_id(), 
+            &token.owner_id,
+            "Predecessor must be the token owner."
+        );
+
+        let approval_id = token.next_approval_id;
+
+        let is_new_approval = token.approved_account_ids.insert(account_id.clone(), approval_id).is_none();
+
+        let storage_used = if is_new_approval {
+            bytes_for_approved_account_id(&account_id)
+        }else{
+            0
+        };
+
+        token.next_approval_id += 1;
+
+        self.tokens_by_id.insert(&token_id, &token);
+
+        refund_deposit(storage_used);
+
+        if let Some(msg) = msg {
+            ext_non_fungible_approval_receiver::nft_on_approve(
+                token_id, 
+                token.owner_id, 
+                approval_id, msg, 
+                account_id, 
+                NO_DEPOSIT, 
+                env::prepaid_gas() - GAS_FOR_NFT_APPROVE
+            )
+            .as_return();
+        }
+
+
+
+    }
+
+    //check if the passed in account has access to approve the token ID
+	fn nft_is_approved(
+        &self,
+        token_id: TokenId,
+        approved_account_id: AccountId,
+        approval_id: Option<u64>,
+    ) -> bool {
+        let token = self.tokens_by_id.get(&token_id).expect("No token");
+
+        let approval = token.approved_account_ids.get(&approved_account_id);
+
+        if let Some(approval) = approval{
+            if let Some(approval_id) = approval_id{
+                approval_id == *approval
+            }else{
+                true
+            }
+        }else{
+            false
+        }
+    }
+
+    //revoke a specific account from transferring the token on your behalf 
+    #[payable]
+    fn nft_revoke(&mut self, token_id: TokenId, account_id: AccountId) {
+        
+        assert_one_yocto();
+
+        let mut token = self.tokens_by_id.get(&token_id).expect("No token");
+
+        let predecessor_account_id = env::predecessor_account_id();
+
+        assert_eq!(&token.owner_id, &predecessor_account_id);
+
+        if token.approved_account_ids.remove(&account_id).is_some(){
+            refund_approved_account_ids_iter(predecessor_account_id, [account_id].iter());
+            self.tokens_by_id.insert(&token_id, &token);
+        }
+
+    }
+
+    //revoke all accounts from transferring the token on your behalf
+    #[payable]
+    fn nft_revoke_all(&mut self, token_id: TokenId) {
+        
+        assert_one_yocto();
+
+        let mut token = self.tokens_by_id.get(&token_id).expect("No token");
+
+        let predecessor_account_id = env::predecessor_account_id();
+
+        assert_eq!(&predecessor_account_id, &token.owner_id);
+
+        if !token.approved_account_ids.is_empty(){
+            refund_approved_account_ids(predecessor_account_id, &token.approved_account_ids);
+            token.approved_account_ids.clear();
+            self.tokens_by_id.insert(&token_id, &token);
+        }
+    }
+}
---
+++ nft-contract/src/nft_core.rs
@@ -0,0 +1,209 @@
+use crate::*;
+use near_sdk::{ext_contract, Gas, PromiseResult, assert_one_yocto};
+
+const GAS_FOR_RESOLVE_TRANSFER: Gas = Gas(10_000_000_000_000);
+const GAS_FOR_NFT_TRANSFER_CALL: Gas = Gas(25_000_000_000_000 + GAS_FOR_RESOLVE_TRANSFER.0);
+const MIN_GAS_FOR_NFT_TRANSFER_CALL: Gas = Gas(100_000_000_000_000);
+const NO_DEPOSIT: Balance = 0;
+
+pub trait NonFungibleTokenCore {
+    //transfers an NFT to a receiver ID
+    fn nft_transfer(
+        &mut self,
+        receiver_id: AccountId,
+        token_id: TokenId,
+        approval_id: Option<u64>,
+        memo: Option<String>,
+    );
+
+    //transfers an NFT to a receiver and calls a function on the receiver ID's contract
+    /// Returns `true` if the token was transferred from the sender's account.
+    fn nft_transfer_call(
+        &mut self,
+        receiver_id: AccountId,
+        token_id: TokenId,
+        approval_id: Option<u64>,
+        memo: Option<String>,
+        msg: String,
+    ) -> PromiseOrValue<bool>;
+
+    //get information about the NFT token passed in
+    fn nft_token(&self, token_id: TokenId) -> Option<JsonToken>;
+}
+
+#[ext_contract(ext_non_fungible_token_receiver)]
+trait NonFungibleTokenReceiver {
+    //Method stored on the receiver contract that is called via cross contract call when nft_transfer_call is called
+    /// Returns `true` if the token should be returned back to the sender.
+    fn nft_on_transfer(
+        &mut self,
+        sender_id: AccountId,
+        previous_owner_id: AccountId,
+        token_id: TokenId,
+        msg: String,
+    ) -> Promise;
+}
+
+#[ext_contract(ext_self)]
+trait NonFungibleTokenResolver {
+    /*
+        resolves the promise of the cross contract call to the receiver contract
+        this is stored on THIS contract and is meant to analyze what happened in the cross contract call when nft_on_transfer was called
+        as part of the nft_transfer_call method
+    */
+    fn nft_resolve_transfer(
+        &mut self,
+        authorized_id: Option<String>,
+        owner_id: AccountId,
+        receiver_id: AccountId,
+        token_id: TokenId,
+        approved_account_ids: HashMap<AccountId,u64>,
+        memo: Option<String>
+    ) -> bool;
+}
+
+/*
+    resolves the promise of the cross contract call to the receiver contract
+    this is stored on THIS contract and is meant to analyze what happened in the cross contract call when nft_on_transfer was called
+    as part of the nft_transfer_call method
+*/ 
+trait NonFungibleTokenResolver {
+    fn nft_resolve_transfer(
+        &mut self,
+        authorized_id: Option<String>,
+        owner_id: AccountId,
+        receiver_id: AccountId,
+        token_id: TokenId,
+        approved_account_ids: HashMap<AccountId,u64>,
+        memo: Option<String>
+    ) -> bool;
+}
+
+#[near_bindgen]
+impl NonFungibleTokenCore for Contract {
+
+    //implementation of the nft_transfer method. This transfers the NFT from the current owner to the receiver. 
+    #[payable]
+    fn nft_transfer(
+        &mut self,
+        receiver_id: AccountId,
+        token_id: TokenId,
+        approval_id: Option<u64>,
+        memo: Option<String>,
+    ) {
+        assert_one_yocto();
+        
+        let sender_id = env::predecessor_account_id();
+
+        let previous_token = self.internal_transfer(&sender_id, &receiver_id, &token_id, approval_id, memo);
+
+        refund_approved_account_ids(previous_token.owner_id.clone(), &previous_token.approved_account_ids);
+    }
+
+    //implementation of the transfer call method. This will transfer the NFT and call a method on the reciver_id contract
+    #[payable]
+    fn nft_transfer_call(
+        &mut self,
+        receiver_id: AccountId,
+        token_id: TokenId,
+        approval_id: Option<u64>,
+        memo: Option<String>,
+        msg: String,
+    ) -> PromiseOrValue<bool> {
+
+        assert_one_yocto();
+
+        let attached_gas = env::prepaid_gas();
+
+        assert!(
+            attached_gas >= MIN_GAS_FOR_NFT_TRANSFER_CALL,
+            "You cannot attach less than {:?} Gas to nft_transfer_call", 
+            MIN_GAS_FOR_NFT_TRANSFER_CALL
+        );
+
+        let sender_id = env::predecessor_account_id();
+
+        let previous_token = self.internal_transfer(&sender_id, &receiver_id, &token_id, approval_id, memo.clone());
+
+        let mut authorized_id = None;
+        if sender_id != previous_token.owner_id {
+            authorized_id = Some(sender_id.to_string());
+        }
+
+        ext_non_fungible_token_receiver::nft_on_transfer(sender_id, previous_token.owner_id.clone(), token_id.clone(), msg, receiver_id.clone(), NO_DEPOSIT, env::prepaid_gas() - GAS_FOR_NFT_TRANSFER_CALL)
+        .then(ext_self::nft_resolve_transfer(authorized_id, previous_token.owner_id, receiver_id, token_id, previous_token.approved_account_ids, memo, env::current_account_id(), NO_DEPOSIT, GAS_FOR_RESOLVE_TRANSFER)).into()
+    }
+
+    //get the information for a specific token ID
+    fn nft_token(&self, token_id: TokenId) -> Option<JsonToken> {
+        if let Some(token) = self.tokens_by_id.get(&token_id){
+            let metadata = self.token_metadata_by_id.get(&token_id).unwrap();
+
+            Some(JsonToken {token_id, owner_id: token.owner_id, metadata, approved_account_ids: token.approved_account_ids, royalty: token.royalty})
+        }else{
+            None
+        }
+    }
+}
+
+#[near_bindgen]
+impl NonFungibleTokenResolver for Contract {
+    //resolves the cross contract call when calling nft_on_transfer in the nft_transfer_call method
+    //returns true if the token was successfully transferred to the receiver_id
+    #[private]
+    fn nft_resolve_transfer(
+        &mut self,
+        authorized_id: Option<String>,
+        owner_id: AccountId,
+        receiver_id: AccountId,
+        token_id: TokenId,
+        approved_account_ids: HashMap<AccountId,u64>,
+        memo: Option<String>
+    ) -> bool {
+        if let PromiseResult::Successful(value) = env::promise_result(0){
+            if let Ok(return_token) = near_sdk::serde_json::from_slice::<bool>(&value){
+                if !return_token {
+                    refund_approved_account_ids(owner_id, &approved_account_ids);
+                    return true;
+                }
+            }
+        }
+        let mut token = if let Some(token) = self.tokens_by_id.get(&token_id){
+            if token.owner_id != receiver_id{
+                refund_approved_account_ids(owner_id, &approved_account_ids);
+                return true;
+            }
+            token
+        }else{
+            refund_approved_account_ids(owner_id, &approved_account_ids);
+            return true;
+        };
+
+        self.internal_remove_token_form_owner(&receiver_id, &token_id);
+        self.internal_add_token_to_owner(&owner_id, &token_id);
+
+        token.owner_id = owner_id.clone();
+
+        refund_approved_account_ids(receiver_id.clone(), &token.approved_account_ids);
+
+        token.approved_account_ids = approved_account_ids;
+
+        self.tokens_by_id.insert(&token_id, &token);
+
+        let nft_transfer_log: EventLog = EventLog{
+            standard: NFT_STANDARD_NAME.to_string(),
+            version: NFT_METADATA_SPEC.to_string(),
+            event: EventLogVariant::NftTransfer(vec![NftTransferLog{
+                authorized_id,
+                old_owner_id: receiver_id.to_string(),
+                new_owner_id: owner_id.to_string(),
+                token_ids: vec![token_id.to_string()],
+                memo
+            }])
+        };
+
+        env::log_str(&nft_transfer_log.to_string());
+
+        false
+    }
+}
